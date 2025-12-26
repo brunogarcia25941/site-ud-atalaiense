41203fb99723d44c76724631d550df552c253faf
brunogarcia25941 <aluno25941@ipt.pt>
2025-12-26 23:13:19 +0000

    Adição de Equipa Técnica e Preparação do site para publicacao em novo dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://udatalaiense.pt',
   vite: {
     plugins: [tailwindcss()]
   }
